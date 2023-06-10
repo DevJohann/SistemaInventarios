2065c5067676a4c7582ff79063b7388beb07ccbc
Maubg2 <mauriciobel2002@gmail.com>
2023-06-10 02:22:45 -0500

    Registro

--- src/main/java/com/unbosque/dao/impl/UserDAOImpl.java
+++ src/main/java/com/unbosque/dao/impl/UserDAOImpl.java
@@ -5,17 +5,21 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import com.unbosque.bean.RegisterBean;
 import com.unbosque.dao.UserDAO;
 import com.unbosque.entity.User;
 import com.unbosque.util.HibernateUtil;
 
 public class UserDAOImpl implements UserDAO {
+	
+	private RegisterBean rb = new RegisterBean();
 
 	@Override
 	public User retrieveUser(String username, String password) {
 		Session sesion = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = sesion.beginTransaction();
-		List response = sesion.createQuery("FROM User WHERE login='" + username + "' AND psswd='" + password + "'")
+		String encryptedPassword = rb.encryptPassword(password, "MD5");
+		List response = sesion.createQuery("FROM User WHERE login='" + username + "' AND psswd='" + encryptedPassword + "'")
 				.list();
 		if (response.size() == 1) {
 			return (User) response.get(0);
